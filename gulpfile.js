d6410a15d75da8147ffb84fc7fe20a43bb1db202
Simon Aspinall <simon.aspinall@optionis.co.uk>
2017-04-20 14:26:58 +0100

    Amend the styling of the watch button and the profile watch data

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,11 @@ var sass = require('gulp-sass')
 var cleanCSS = require('gulp-clean-css')
 var concat = require('gulp-concat')
 
+
 var jsSrc = './js/src/**/*.js'
 var sassSrc = './css/sass/*.scss'
 var skeletonSrc = './node_modules/skeleton-css/css/*.css'
+var fontAwesomeSrc = './node_modules/font-awesome/css/font-awesome.min.css'
 
 gulp.task('scripts', function() {
     return gulp.src(jsSrc)
@@ -15,7 +17,7 @@ gulp.task('scripts', function() {
 })
 
 gulp.task('styles', function() {
-    gulp.src(sassSrc)
+    return gulp.src(sassSrc)
         .pipe(concat('styles.min.css'))
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./public/css'))
@@ -28,8 +30,13 @@ gulp.task('skeleton', () => {
         .pipe(gulp.dest('./public/css'))
 })
 
+gulp.task('font-awesome', () => {
+    return gulp.src(fontAwesomeSrc)
+        .pipe(gulp.dest('./public/css'))
+})
+
 gulp.task('automate', function() {
     gulp.watch([sassSrc, jsSrc], ['scripts', 'styles'])
 })
 
-gulp.task('default', ['scripts', 'styles', 'skeleton', 'automate'])
+gulp.task('default', ['scripts', 'styles', 'skeleton', 'font-awesome', 'automate'])
